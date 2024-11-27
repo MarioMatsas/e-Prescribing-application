0f4da0f35238cd78322ded4e770ad0d3e07c1ced
DrOctapus <andrpanago333@gmail.com>
2024-11-27 16:28:00 +0200

    updated constructor to include product

--- docs/target/classes/ProductQuantity.java
+++ docs/target/classes/ProductQuantity.java
@@ -2,8 +2,8 @@ public class ProductQuantity {
     private PharmacudicalProduct product;
     public final Integer quantity;
 
-
-    public ProductQuantity(Integer quantity) {
+    public ProductQuantity(PharmacudicalProduct product, Integer quantity) {
+        this.product = product;
         this.quantity = quantity;
     }
 
